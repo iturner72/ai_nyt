ad5038ea82fde355631e04ae26523c3a4e66265d
Ian Turner <ian96turner@gmail.com>
2024-03-13 00:44:20 -0600

    fix mobile navbar

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -12,19 +12,21 @@ export default function NavBar({ channels, currentChannelIndex, setCurrentChanne
   };
 
   return (
-    <div className="sticky w-10/12 top-0 z-20">
-      <div className="flex items-center justify-between bg-stone-100 font-sans border-b border-neutral-200">
-        {channels.map((channel, index) => (
-          <button
-            key={index}
-            onClick={() => handleTabClick(index)}
-            className={`w-full text-left h-10 border-t border-r p-2 border-stone-700 last:border-r first:border-l font-semibold border-b ${
-              currentChannelIndex === index ? 'bg-stone-200 border-stone-500 text-stone-900' : 'bg-stone-900 text-stone-50'
-            }`}
-          >
-            /{channel.split('/').pop()}
-          </button>
-        ))}
+    <div className="sticky top-0 z-20 w-full flex justify-center">
+      <div className="w-10/12 overflow-x-auto">
+        <div className="flex items-center justify-between bg-stone-100 font-sans border-b border-neutral-200">
+          {channels.map((channel, index) => (
+            <button
+              key={index}
+              onClick={() => handleTabClick(index)}
+              className={`w-full text-left h-10 border-t border-r p-2 border-stone-700 last:border-r first:border-l font-semibold border-b ${
+                currentChannelIndex === index ? 'bg-stone-200 border-stone-500 text-stone-900' : 'bg-stone-900 text-stone-50'
+              }`}
+            >
+              /{channel.split('/').pop()}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
