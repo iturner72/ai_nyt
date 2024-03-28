d854cf6dc10aa78fd70cdbf48696d800e4359efb
iturner72 <ian96turner@gmail.com>
2024-03-27 20:23:20 -0600

    add skeletons to user cast pfps

--- client/src/components/Cast.tsx
+++ client/src/components/Cast.tsx
@@ -30,73 +30,75 @@ export default function CastEntry({ cast, index }: CastProps) {
   const [userData, setUserData] = useState<UserData | null>(null);
   const [userPfpData, setUserPfpData] = useState<UserData | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [pfpLoading, setPfpLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchUserData = async () => {
       setLoading(true);
       try {
-        const [usernameResponse, pfpResponse] = await Promise.all([
-          axios.get(`https://${config2.serverBaseUrl}/userDataByFid`, {
-            params: {
-              fid: cast.data?.fid,
-              user_data_type: 6,
-            },
-          }),
-          axios.get(`https://${config2.serverBaseUrl}/userDataByFid`, {
-            params: {
-              fid: cast.data?.fid,
-              user_data_type: 1,
-            },
-          }),
-        ]);
+        const usernameResponse = await axios.get(`https://${config2.serverBaseUrl}/userDataByFid`, {
+          params: {
+            fid: cast.data?.fid,
+            user_data_type: 6,
+          },
+        });
 
         if (usernameResponse.status === 200 && usernameResponse.data) {
           setUserData(usernameResponse.data);
+          setLoading(false);
         }
-        if (usernameResponse.status === 200 && pfpResponse.data) {
+
+        const pfpResponse = await axios.get(`https://${config2.serverBaseUrl}/userDataByFid`, {
+          params: {
+            fid: cast.data?.fid,
+            user_data_type: 1,
+          },
+        });
+
+        if (pfpResponse.status === 200 && pfpResponse.data) {
           setUserPfpData(pfpResponse.data);
         }
+        setPfpLoading(false);
       } catch (error) {
         console.error("Error fetching user data:", error);
-      } finally {
         setLoading(false);
+        setPfpLoading(false);
       }
     };
 
     fetchUserData();
   }, [cast.data?.fid]);
 
-return cast.data ? (
-  <div key={index} className="flex flex-col text-left p-4 pl-6 bg-stone-200 border border-stone-500">
-    <div className="flex-grow">
-      <p className="alumni-sans-regular text-lg md:text-xl break-words hyphens-auto" style={{ lineHeight: 1.172 }}>
-        {cast.data?.castAddBody ? cast.data?.castAddBody?.text : 'N/A'}
-      </p>
-    </div>
-
-<div className="flex flex-row items-center justify-between mt-4">
-  {loading ? (
-    <div>Loading...</div>
-  ) : (
-    <>
-      <div className="flex items-center">
-        {userPfpData?.data?.userDataBody?.value && (
-          <img
-            src={userPfpData.data.userDataBody.value}
-            alt="User PFP"
-            className="w-8 h-8 rounded-full mr-2"
-          />
-        )}
-        <h3 className="alumni-sans-bold text-stone-500 text-lg md:text-xl">
-          @{userData?.data?.userDataBody?.value || "No username"}
-        </h3>
+  return cast.data ? (
+    <div key={index} className="flex flex-col text-left p-4 pl-6 bg-stone-200 border border-stone-500">
+      <div className="flex-grow">
+        <p className="alumni-sans-regular text-lg md:text-xl break-words hyphens-auto" style={{ lineHeight: 1.172 }}>
+          {cast.data?.castAddBody ? cast.data?.castAddBody?.text : 'N/A'}
+        </p>
       </div>
-      <img src="/images/fc.jpeg" alt="Farcaster" className="w-6 h-6 ml-2" />
-    </>
-  )}
-</div>
-
-
-  </div>
-) : null;
+      <div className="flex flex-row items-center justify-between mt-4">
+        <div className="flex items-center">
+          {pfpLoading ? (
+            <div className="w-8 h-8 rounded-full bg-gray-300 animate-pulse mr-2" />
+          ) : userPfpData?.data?.userDataBody?.value ? (
+            <img
+              src={userPfpData.data.userDataBody.value}
+              alt="User PFP"
+              className="w-8 h-8 rounded-full mr-2"
+            />
+          ) : (
+            <div className="w-8 h-8 rounded-full bg-gray-300 mr-2" />
+          )}
+          <h3 className="alumni-sans-bold text-stone-500 text-lg md:text-xl">
+            {loading ? (
+              <div className="w-24 h-6 bg-gray-300 animate-pulse" />
+            ) : (
+              `@${userData?.data?.userDataBody?.value || "No username"}`
+            )}
+          </h3>
+        </div>
+        <img src="/images/fc.jpeg" alt="Farcaster" className="w-6 h-6 ml-2" />
+      </div>
+    </div>
+  ) : null;
 }
