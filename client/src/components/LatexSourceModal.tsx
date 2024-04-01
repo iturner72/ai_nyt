fa470f351055f79a50b6bf550717c58bde62a7bf
Ian Turner <ian96turner@gmail.com>
2024-04-01 00:20:20 -0600

    change from download to open pdf (wtf was i thinking)

--- client/src/components/LatexSourceModal.tsx
+++ client/src/components/LatexSourceModal.tsx
@@ -5,28 +5,23 @@ interface LatexSourceModalProps {
 }
 
 const LatexSourceModal: React.FC<LatexSourceModalProps> = ({ closeModal }) => {
-
+  const handleOpenPDF = () => {
+    window.open('/documents/notes.pdf', '_blank');
+  };
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
       <div className="bg-white p-4 rounded-lg shadow-lg w-10/12 md:w-4/12">
         <h2 className="text-3xl font-bold mb-4">LaTeX Source Code</h2>
         <pre className="bg-gray-100 text-xs md:text-xl p-4 rounded-md mb-4">
-          {
-
-`\\documentclass{article}
+          {`\\documentclass{article}
 \\begin{document}
-
-\\textbf{\\textit{article(s) local   
-\  storage cleared every 2 hrs btw}}.
-
+\\textbf{\\textit{article(s) local
+\\ storage cleared every 2 hrs btw}}.
 \\section*{What's This?}
-
-\This (the PDF) is a changelog
-\  that I'll update periodically.
-\\end{document}`
-
-          }
+\\This (the PDF) is a changelog
+\\ that I'll update periodically.
+\\end{document}`}
         </pre>
         <div className="flex flex-col items-center justify-center">
           <a
@@ -36,19 +31,18 @@ const LatexSourceModal: React.FC<LatexSourceModalProps> = ({ closeModal }) => {
           >
             Github
           </a>
-            <div className="flex flex-row items-center justify-center pt-2">
-              <a
-                href="/documents/notes.pdf"
-                download="change_log.pdf"
-                className="bg-stone-600 hover:bg-stone-800 alumni-sans-regular text-xl text-white px-4 py-2 rounded-md"
-              >
-                Download PDF
-              </a>
-              <button
-                onClick={closeModal}
-                className="ml-4 alumni-sans-regular text-xl bg-gray-300 text-stone-800 px-4 py-2 rounded-md"
-              >
-                Close
+          <div className="flex flex-row items-center justify-center pt-2">
+            <button
+              onClick={handleOpenPDF}
+              className="bg-stone-600 hover:bg-stone-800 alumni-sans-regular text-xl text-white px-4 py-2 rounded-md"
+            >
+              Open PDF
+            </button>
+            <button
+              onClick={closeModal}
+              className="ml-4 alumni-sans-regular text-xl bg-gray-300 text-stone-800 px-4 py-2 rounded-md"
+            >
+              Close
             </button>
           </div>
         </div>
