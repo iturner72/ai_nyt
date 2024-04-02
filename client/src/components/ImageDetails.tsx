12594d36ae744c933e860cfa0d1b64c7e0912991
iturner72 <ian96turner@gmail.com>
2024-04-01 20:01:05 -0600

    fix image detail dimensions

--- client/src/components/ImageDetails.tsx
+++ client/src/components/ImageDetails.tsx
@@ -8,6 +8,12 @@ interface ImageDetailsProps {
 const ImageDetails: React.FC<ImageDetailsProps> = ({ src, alt }) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const closeModal = (event: React.MouseEvent) => {
+    if ((event.target as HTMLElement).id === 'overlay') {
+      setIsOpen(false);
+    }
+  };
+
   return (
     <>
       <img
@@ -17,10 +23,16 @@ const ImageDetails: React.FC<ImageDetailsProps> = ({ src, alt }) => {
         onClick={() => setIsOpen(true)}
       />
       {isOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 z-150 flex justify-center items-center">
-          <div className="bg-indigo-500 bg-opacity-50 p-14">
-            <img src={src} alt={alt} className="max-w-full max-h-full" />
-            <button onClick={() => setIsOpen(false)} className="mt-2">close</button>
+        <div
+          id="overlay"
+          className="fixed inset-0 bg-black bg-opacity-50 z-150 flex justify-center items-center"
+          onClick={closeModal}
+        >
+          <div
+            className="flex flex-col bg-indigo-500 bg-opacity-50 p-4"
+            onClick={(event) => event.stopPropagation()} 
+          >
+            <img src={src} alt={alt} className="max-w-[80vw] max-h-[80vh] md:max-w-screen-sm md:max-h-[80vh]" />
           </div>
         </div>
       )}
