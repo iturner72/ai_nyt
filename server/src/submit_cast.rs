a64f08defcf684504f6ecfe62964bf7eef8c733a
iturner72 <ian96turner@gmail.com>
2024-04-04 08:28:05 -0700

    create cast still not working, but closer

--- server/src/submit_cast.rs
+++ server/src/submit_cast.rs
@@ -1,25 +1,36 @@
-use std::time::SystemTime;
-use std::time::UNIX_EPOCH;
-
 use actix_web::{post, web, HttpResponse, Responder};
 use blake3;
-use ed25519_dalek::{Keypair, PublicKey, SecretKey, Signer, SigningKey};
+use ed25519_dalek::{PublicKey, Signature};
+use hex::decode;
+use message::{CastAddBody, FarcasterNetwork, MessageData, HashScheme, Message, MessageType, SignatureScheme};
 use prost::Message as ProstMessage;
-use hex::FromHex;
-use protobuf::Message as ProtobufMessage;
 use reqwest::Client;
+use serde::Deserialize;
+use std::env;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::message;
 use crate::username_proof;
 
-use message::{CastAddBody, FarcasterNetwork, MessageData, HashScheme, Message, MessageType, SignatureScheme};
-
 const FARCASTER_EPOCH: u64 = 1609459200;
 
+#[derive(Deserialize)]
+struct CastSubmission {
+    cast_add_body: CastAddBody,
+    signature_hex: String,
+    public_key_hex: String,
+}
+
 #[post("/submitCast")]
-async fn submit_cast(body: web::Json<CastAddBody>) -> impl Responder {
+async fn submit_cast(body: web::Json<CastSubmission>) -> impl Responder {
+    let CastSubmission {
+        cast_add_body,
+        signature_hex,
+        public_key_hex,
+    } = body.into_inner();
+
     let fid = 249222;
-    let network = FarcasterNetwork::FARCASTER_NETWORK_MAINNET;
+    let network = FarcasterNetwork::FarcasterNetworkMainnet as i32;
 
     let timestamp = SystemTime::now()
         .duration_since(UNIX_EPOCH)
@@ -28,28 +39,27 @@ async fn submit_cast(body: web::Json<CastAddBody>) -> impl Responder {
         .checked_sub(FARCASTER_EPOCH)
         .expect("Invalid timestamp") as u32;
 
-    let mut cast_add = CastAddBody {
-      ..Default::default()
-    };
-
     let mut msg_data = MessageData {
-        type_: MessageType::MessageTypeCastAdd as i32,
+        r#type: MessageType::MessageTypeCastAdd as i32,
         fid,
         timestamp,
         network,
-        body: Some(message::Body::CastAddBody(cast_add)),
+        body: Some(message::message_data::Body::CastAddBody(cast_add_body)),
     };
 
     // Serialize MessageData
-    let mut msg_data_bytes = vec![];
+    let mut msg_data_bytes = Vec::new(); 
     msg_data.encode(&mut msg_data_bytes).unwrap();
 
     // Hash MessageData
-    let hash = blake3::hash(&msg_data_bytes);
-    let hash_bytes = hash.as_bytes()[..20].to_vec();
+    let hash_bytes = blake3::hash(&msg_data_bytes).as_bytes()[..20].to_vec(); 
 
+    let signature_bytes = decode(&signature_hex).expect("Failed to decode hex signature");
+    let public_key_bytes = decode(&public_key_hex).expect("Failed to decode hex public key");
 
-    // TODO Signature and singer logic
+    // Create ed25519_dalek signature and public key types from decoded bytes
+    let signature = Signature::from_bytes(&signature_bytes).expect("Invalid signature format");
+    let public_key = PublicKey::from_bytes(&public_key_bytes).expect("Invalid public key format");
 
     let message = Message {
         data: Some(msg_data),
@@ -57,9 +67,28 @@ async fn submit_cast(body: web::Json<CastAddBody>) -> impl Responder {
         hash_scheme: HashScheme::HashSchemeBlake3 as i32,
         signature: signature.to_bytes().to_vec(),
         signature_scheme: SignatureScheme::SignatureSchemeEd25519 as i32,
-        signer: keypair.public.to_bytes().to_vec(),
-        ..Default::default()
+        signer: public_key.to_bytes().to_vec(), 
     };
 
-    HttpResponse::Ok().json(message)
+    let mut buf = Vec::new();
+    message.encode(&mut buf).expect("Message encoding failed");
+
+    let client = Client::new();
+
+    let hubble_url = env::var("HUBBLE_URL").expect("HUBBLE_URL must be set");
+    let mut url = format!("{}:2281/v1/submitMessage", hubble_url);
+
+    let res = client
+        .post(url)
+        .header("Content-Type", "application/octet-stream")
+        .body(buf)
+        .send()
+        .await;
+
+    match response {
+        Ok(res) if res.status().is_success() => HttpResponse::Ok().json("Cast sumbitted successfully"),
+        Ok(res) => HttpResponse::BadRequest().body(format!("Failed to send the message. HTTP status: {}", res.status())),
+        Err(err) => HttpResponse::InternalServerError().body(format!("HTTP request failed: {}", err)),
+    }
 }
+
