a88799628c823ba2f5e0d5576989b8757ae9a268
iturner72 <ian96turner@gmail.com>
2024-05-13 22:36:28 -0700

    [wip] testing new article struct for db insertion

--- server/src/database/db.rs
+++ server/src/database/db.rs
@@ -1,18 +1,25 @@
 use diesel::prelude::*;
+use crate::schema::articles;
+use diesel::result::Error as DieselError;
 use crate::database::models::article::Article;
 use crate::schema::articles::dsl::*;
 
-pub fn create_article(conn: &mut PgConnection, user_id_param: i64, article_title: &str, article_content: &str) -> QueryResult<Article> {
-    let new_article = Article {
-        id: 0,
+pub fn create_article(conn: &mut PgConnection, user_id_param: i64, article_title: &str, article_content: &str) -> Result<Article, DieselError> {
+    let new_article = NewArticle {
         user_id: user_id_param,
         title: article_title.to_string(),
         content: article_content.to_string(),
-        created_at: chrono::Utc::now().naive_utc(),
-        is_default: false,
     };
 
     diesel::insert_into(articles)
         .values(&new_article)
         .get_result(conn)
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = articles)]
+struct NewArticle {
+    user_id: i64,
+    title: String,
+    content: String,
 }
